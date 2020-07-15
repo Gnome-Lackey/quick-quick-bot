a0c0b3110b390583c7d4faa62bf34c2aa7d00418
Matthew_Pfister <matthew.pfister@slalom.com>
2020-07-14 20:26:20 -0500

    code cleanup

--- src/bot.ts
+++ src/bot.ts
@@ -2,9 +2,14 @@ import { Client, Message } from "discord.js";
 
 import NameUtility from "./name.utility";
 
-const qq = new Client();
+import { NonHumanRaces } from "./types";
 
-const qqCommandRegex = /^qq!/;
+const qq = new Client();
+const nameUtility = new NameUtility();
+
+const COMMAND_PREFIX = "qq!";
+const COMMAND_PREFIX_REGEX = /^qq!/;
+const NON_HUMAN_RACES = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Halfling", "Orc", "Tiefling"];
 
 qq.once("ready", () => {
   console.log("Howdy boss! Quick-quick Gnome Lackey at your service.");
@@ -13,60 +18,34 @@ qq.once("ready", () => {
 qq.on("message", (message: Message) => {
   const text = message.content;
 
-  if (!qqCommandRegex.test(text)) {
+  if (!COMMAND_PREFIX_REGEX.test(text)) {
     // Ignore commands that don't begin with the quick-quick command.
     return;
   }
 
   message.channel.send(`I'm on it boss!`);
 
-  const [, argumentList] = text.split("qq!");
+  const [, argumentList] = text.split(COMMAND_PREFIX);
   const args = argumentList.split(" ").filter((arg) => !!arg);
   const argumentCount = args.length;
 
   if (argumentCount === 0) {
-    const nameUtility = new NameUtility();
+    const femaleNameMessage = NON_HUMAN_RACES.reduce(
+      (fullText, race) => `${fullText}
+      **${race}:** ${nameUtility.buildNonHumanNames(1, "female", race as NonHumanRaces)}`,
+      `*Female Names:*
+      **Humans:** ${nameUtility.buildHumanNames(1, "female")}`
+    );
 
-    const humanFemaleFirstNames = nameUtility.buildHumanNames(1, "female");
-    const humanMaleFirstNames = nameUtility.buildHumanNames(1, "male");
-    const dragonbornFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "dragonborn");
-    const dragonbornMaleFirstNames = nameUtility.buildNonHumanNames(1, "male", "dragonborn");
-    const dwarfFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "dwarf");
-    const dwarfMaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "dwarf");
-    const elfFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "elf");
-    const elfMaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "elf");
-    const gnomeFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "gnome");
-    const gnomeMaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "gnome");
-    const halflingFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "halfling");
-    const halflingMaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "halfling");
-    const orcFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "orc");
-    const orcMaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "orc");
-    const tieflingFemaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "tiefling");
-    const tieflingMaleFirstNames = nameUtility.buildNonHumanNames(1, "female", "tiefling");
+    const maleNameMessage = NON_HUMAN_RACES.reduce(
+      (fullText, race) => `${fullText}
+      **${race}:** ${nameUtility.buildNonHumanNames(1, "male", race as NonHumanRaces)}`,
+      `*Male Names:*
+      **Humans:** ${nameUtility.buildHumanNames(1, "male")}`
+    );
 
-    message.channel.send(`
-      *Female Names:*
-      **Dragonborn:** ${dragonbornFemaleFirstNames}
-      **Dwarf:** ${dwarfFemaleFirstNames}
-      **Elf:** ${elfFemaleFirstNames}
-      **Gnome:** ${gnomeFemaleFirstNames}
-      **Halfling:** ${halflingFemaleFirstNames}
-      **Human:** ${humanFemaleFirstNames}
-      **Orc:** ${orcFemaleFirstNames}
-      **Tiefling:** ${tieflingFemaleFirstNames}
-    `);
-
-    message.channel.send(`
-      *Male Names:*
-      **Dragonborn:** ${dragonbornMaleFirstNames}
-      **Dwarf:** ${dwarfMaleFirstNames}
-      **Elf:** ${elfMaleFirstNames}
-      **Gnome:** ${gnomeMaleFirstNames}
-      **Halfling:** ${halflingMaleFirstNames}
-      **Human:** ${humanMaleFirstNames}
-      **Orc:** ${orcMaleFirstNames}
-      **Tiefling:** ${tieflingMaleFirstNames}
-    `);
+    message.channel.send(femaleNameMessage);
+    message.channel.send(maleNameMessage);
   }
 });
 
