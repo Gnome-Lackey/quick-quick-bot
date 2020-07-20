209b3e0d03c4b8355d39b016a4e4525d3a33a1e8
Matthew_Pfister <matthew.pfister@slalom.com>
2020-07-20 09:56:58 -0500

    fixing toProperNoun to not capitalize articles, conjunctions, and prepositions

--- src/utilities/qq.utility.ts
+++ src/utilities/qq.utility.ts
@@ -1,8 +1,29 @@
+const WORDS_TO_SKIP = [
+  "a",
+  "an",
+  "the",
+  "at",
+  "by",
+  "for",
+  "in",
+  "of",
+  "on",
+  "to",
+  "up",
+  "and",
+  "as",
+  "but",
+  "or",
+  "nor"
+];
+
 export const toProperNoun = (text: string): string => {
   const words = text.split(" ");
 
   return words
     .reduce((properNouns, word) => {
+      if (WORDS_TO_SKIP.includes(word)) return [...properNouns, word];
+
       const firstLetter = word.charAt(0).toUpperCase();
 
       return [...properNouns, `${firstLetter}${word.slice(1)}`];
